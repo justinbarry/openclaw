4063d0d9eff718e9e6474989a30f34cff5c4c575
exe.dev user <exedev@santaclaws.exe.xyz>
2026-04-27 19:34:47 +0000

    Fix Linear tool typebox imports to use repo alias

--- src/agents/tools/linear-helpers.ts
+++ src/agents/tools/linear-helpers.ts
@@ -1,4 +1,4 @@
-import { Type } from "@sinclair/typebox";
+import { Type } from "typebox";
 import { optionalStringEnum } from "../schema/typebox.js";
 import { type AnyAgentTool, jsonResult, readStringParam } from "./common.js";
 
